c4ae82aecc0c52a00f35af9aa719d213b67f5ecd
aahrabian <faridsaniee@gmail.com>
2017-10-25 10:10:21 +0330

    Create Activities For CustomJourney

---
+++ app/src/main/java/com/iranplanner/tourism/iranplanner/ui/activity/customJourney/CreateCustomJourney.java
@@ -0,0 +1,18 @@
+package com.iranplanner.tourism.iranplanner.ui.activity.customJourney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iranplanner.tourism.iranplanner.R;
+
+public class CreateCustomJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_custom_journey);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/iranplanner/tourism/iranplanner/ui/activity/customJourney/CustomJourney.java
@@ -0,0 +1,18 @@
+package com.iranplanner.tourism.iranplanner.ui.activity.customJourney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iranplanner.tourism.iranplanner.R;
+
+public class CustomJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_journey);
+
+
+
+    }
+}
